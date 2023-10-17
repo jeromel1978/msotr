812a62e277c563495e63f269e31649a95e14b9ee
jeromel1978 <jeromelosorata@hotmail.com>
2023-10-17 11:28:57 -0500

    Removed Command Line

--- lib/index.ts
+++ lib/index.ts
@@ -205,15 +205,15 @@ const msotr = async ({ URL, Local, Replacements, Out }: Props) => {
 };
 
 export default msotr;
-for (let i = 0; i < process.argv.length; ++i) {
-  console.log(`index ${i} argument -> ${process.argv[i]}`);
-}
-if (!!process.argv[3]) {
-  const f = fs.readFileSync(process.argv[2]);
-  const Replacements = JSON.parse(f.toString());
-  if (process.argv[3].startsWith("http"))
-    msotr({ URL: process.argv[3], Replacements: Replacements, Out: process.argv[4] });
-  else msotr({ Local: process.argv[3], Replacements: Replacements, Out: process.argv[4] });
-} else {
-  console.log("USEAGE: npm run start [ReplacementJSON] [Template.pptx] [Output.pptx]");
-}
+// for (let i = 0; i < process.argv.length; ++i) {
+//   console.log(`index ${i} argument -> ${process.argv[i]}`);
+// }
+// if (!!process.argv[3]) {
+//   const f = fs.readFileSync(process.argv[2]);
+//   const Replacements = JSON.parse(f.toString());
+//   if (process.argv[3].startsWith("http"))
+//     msotr({ URL: process.argv[3], Replacements: Replacements, Out: process.argv[4] });
+//   else msotr({ Local: process.argv[3], Replacements: Replacements, Out: process.argv[4] });
+// } else {
+//   console.log("USEAGE: npm run start [ReplacementJSON] [Template.pptx] [Output.pptx]");
+// }
